ec216523866aae55d6bd1da28605a8d93464bc41
Jessica Natovich <jessicanatovich@yahoo.com>
2021-04-28 22:49:12 +0000

    Automatically backed up by Learn

--- src/components/ClickityClick.js
+++ src/components/ClickityClick.js
@@ -1,33 +1,50 @@
 // Code ClickityClick Component Here
 
+// src/components/ClickityClick.js
 import React from 'react';
  
-class LightSwitch extends React.Component {
+class ClickityClick extends React.Component {
   constructor() {
     super();
  
-    // Initial state is defined
+    // Define the initial state:
     this.state = {
-      toggled: false
+      hasBeenClicked: false
     };
   }
  
-  // when handleClick is called, setState will update the state so that toggle is reversed
   handleClick = () => {
-    this.setState(previousState => {
-      return {
-        toggled: !previousState.toggled
-      }
-    })
-  }
+    // Update our state here...
+  };
  
   render() {
     return (
       <div>
-        <button onClick={this.handleClick}>{this.state.toggled ? "ON" : "OFF"}</button>
+        <p>I have {this.state.hasBeenClicked ? null : 'not'} been clicked!</p>
+        <button onClick={this.handleClick}>Click me!</button>
       </div>
     );
   }
 }
  
-export default LightSwitch;
+export default ClickityClick;
+ 
+// src/index.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+ 
+import ClickityClick from './components/ClickityClick';
+ 
+ReactDOM.render(<ClickityClick />, document.getElementById('root'));
+To update our state, we use this.setState() and pass in an object. This object will get merged with the current state. When the state has been updated, our component re-renders automatically. Handy!
+
+// src/components/ClickityClick.js
+...
+ 
+handleClick = () => {
+  this.setState({
+    hasBeenClicked: true
+  })
+}
+ 
+...
